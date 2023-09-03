3b41ee8d2ed9dffbbfe27fa4cda601959502d795
Facundo David Gonzalez <fac.gonzalez@outlook.com>
2023-09-03 19:45:44 -0300

    add api url to env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [vue()],
+    server: {
+      proxy: {
+        "/api": env.VITE_API_URL,
+      },
     },
-  },
+  };
 });
